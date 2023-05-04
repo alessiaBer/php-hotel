ed9935a4773b5fcdf2c426b49ae73cbdf64894cc
alessiaBer <alexdeyn@gmail.com>
2023-05-04 16:16:47 +0200

    [organize] hotels data in a table

--- index.php
+++ index.php
@@ -60,15 +60,35 @@ $hotels = [
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
-    <h1>PHP Hotels</h1>
-    <?php foreach ($hotels as $hotel) : ?>
-        <ul>
-            <?php foreach ($hotel as $key => $value) : ?>
-                <li><?= $key; ?> : <?= $value ?></li>
-            <?php endforeach ?>
-        </ul>
-    <?php endforeach ?>
+    
+    <div class="container">
+        <h1 class="py-4">PHP Hotels</h1>
+        <table class="table">
+            <thead>
+                <?php foreach ($hotels[0] as $key => $value) : ?>
+                    <th scope="col"><?= ucwords(str_replace('_', ' ', $key)) ?></th>
+                <?php endforeach?>
+            </thead>
+            <tbody>
+                <?php foreach ($hotels as $hotel) : ?>
+                    <tr>
+                        <?php foreach ($hotel as $key => $value) : ?>
+                            <?php if ($value === true) : ?>
+                                <td>Yes</td>
+                            <?php elseif ($value === false) : ?>
+                                <td>No</td>
+                            <?php else : ?>
+                            <td><?= $value ?></td>
+                            <?php endif ?>
+                        <?php endforeach ?>
+                    </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+
+    </div>
 </body>
 </html>
