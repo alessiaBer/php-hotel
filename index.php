8b83e9d450ed658e49e1c23b925f110f5f513a68
alessiaBer <alexdeyn@gmail.com>
2023-05-04 15:33:08 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php 
+/* 
+Descrizione
+Partiamo da questo array di hotel:
+https://www.codepile.net/pile/OEWY7Q1G
+Stampare tutti i nostri hotel con tutti i dati disponibili.
+Iniziate per steps:
+-Prima stampate in pagina i dati, senza preoccuparvi dello stile.
+-Dopo aggiungete Bootstrap e mostrate le informazioni con una tabella.
+Bonus: 1
+Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio.
+Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore)
+
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Hotel</title>
+</head>
+<body>
+    
+</body>
+</html>
