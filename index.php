498448c6ba169e3db0eb44f7ddd41f2679c65b0f
alessiaBer <alexdeyn@gmail.com>
2023-05-04 15:45:20 +0200

    [print] hotels on page

--- index.php
+++ index.php
@@ -12,6 +12,45 @@ Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di fi
 Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore)
 
 */
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
 ?>
 
 <!DOCTYPE html>
@@ -23,6 +62,13 @@ Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto 
     <title>PHP Hotel</title>
 </head>
 <body>
-    
+    <h1>PHP Hotels</h1>
+    <?php foreach ($hotels as $hotel) : ?>
+        <ul>
+            <?php foreach ($hotel as $key => $value) : ?>
+                <li><?= $key; ?> : <?= $value ?></li>
+            <?php endforeach ?>
+        </ul>
+    <?php endforeach ?>
 </body>
 </html>
